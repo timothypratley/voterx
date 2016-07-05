2e49cc15ce5f6cd250a5f19ccbf048aec75b2762
Timothy Pratley <timothypratley@gmail.com>
2016-07-04 23:02:20 -0700

    fixes deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 cd $(dirname $0)
-lein do clean, cljsbuild once min
-cd resources/public
+boot production build target
+cd target
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
