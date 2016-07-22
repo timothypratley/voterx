070c56ba1a24f3f40898f063aa8d5083eddada28
Timothy Pratley <timothypratley@gmail.com>
2016-07-21 23:16:41 -0700

    follow "-0" convention

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   "0.0.0-SNAPSHOT"
   :dependencies
   [[adzerk/boot-cljs "1.7.228-1" :scope "test"]
-   [adzerk/boot-reload "0.4.11" :scope "test"]
+   [adzerk/boot-reload "0.4.12" :scope "test"]
    [pandeiro/boot-http "0.7.3" :scope "test"]
    [org.clojure/clojure "1.9.0-alpha10"]
    [org.clojure/clojurescript "1.9.93"]
@@ -13,6 +13,6 @@
    [reagent "0.6.0-rc"]
    [devcards "0.2.1-7"]
    [cljsjs/d3 "3.5.16-0"]
-   [cljsjs/firebase "3.2.0"]]
+   [cljsjs/firebase "3.2.0-0"]]
   :source-paths
   ["src/cljs" "resources"])
